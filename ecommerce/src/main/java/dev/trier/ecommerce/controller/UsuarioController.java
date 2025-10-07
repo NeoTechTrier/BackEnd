8dbf1ec8871efab5cccd3b2683396ed5162f37ed
Maicon Rocha <atendimento.maiconrs@gmail.com>
2025-10-07 17:16:54 -0300

    Adicionado anotações Rest...

--- ecommerce/src/main/java/dev/trier/ecommerce/controller/UsuarioController.java
+++ ecommerce/src/main/java/dev/trier/ecommerce/controller/UsuarioController.java
@@ -1,4 +1,9 @@
 package dev.trier.ecommerce.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
 public class UsuarioController {
 }
