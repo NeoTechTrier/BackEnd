99bac85eb33c4eb7dba1cc14fe3c5e3f9f60bb50
DaviBeckha <davibeckhauser@hotmail.com>
2025-10-09 11:22:53 -0300

    Feat: Removido Schemas e documentação

--- ecommerce/src/main/java/dev/trier/ecommerce/model/EmpresaModel.java
+++ ecommerce/src/main/java/dev/trier/ecommerce/model/EmpresaModel.java
@@ -1,6 +1,5 @@
 package dev.trier.ecommerce.model;
 
-import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -18,43 +17,32 @@ import java.util.List;
                 @UniqueConstraint(columnNames = {"nmRazao"}),
                 @UniqueConstraint(columnNames = {"nuTelefone"})
         })
-@Schema(description = "Criação da empresa para vincular nos seus produtos")
 public class EmpresaModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Schema(description = "Identificador único da empresa", example = "1")
     private Integer cdEmpresa;
 
     @Column(nullable = false)
-    @Schema(description = "Nome fantasia da empresa", example = "Neo Tech Ltda")
     private String nmFantasia;
 
     @Column(nullable = false)
-    @Schema(description = "Razão social da empresa", example = "Neo Tech Tecnologia Ltda")
     private String nmRazao;
 
     @Column(nullable = false)
-    @Schema(description = "CNPJ da empresa", example = "12.345.678/0001-99")
     private String nuCPNJ;
 
     @Column(nullable = false)
-    @Schema(description = "Telefone da empresa", example = "11987654321")
-    private Integer nuTelefone;
+    private String nuTelefone;
 
     @Column(nullable = false)
-    @Schema(description = "Descrição do endereço da empresa", example = "Rua das Flores, Bairro Central")
     private String dsEndereco;
 
     @Column(nullable = false)
-    @Schema(description = "Número do endereço", example = "123")
     private String nuEndereco;
 
-
-    @Schema(description = "Flag indicando se a empresa está ativa", example = "S")
     private String flAtivo = "S";
 
     @OneToMany(mappedBy = "empresa", cascade = CascadeType.ALL, orphanRemoval = true)
-    @Schema(description = "Lista de produtos fornecidos pela empresa")
     private List<ProdutoModel> produtos;
 }
--- ecommerce/src/main/java/dev/trier/ecommerce/service/EmpresaService.java
+++ ecommerce/src/main/java/dev/trier/ecommerce/service/EmpresaService.java
@@ -1,7 +1,17 @@
 package dev.trier.ecommerce.service;
 
+import dev.trier.ecommerce.repository.EmpresaRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmpresaService {
+
+    private final EmpresaRepository empresaRepository;
+
+    public EmpresaService(EmpresaRepository empresaRepository) {
+        this.empresaRepository = empresaRepository;
+    }
+
+
+
 }
