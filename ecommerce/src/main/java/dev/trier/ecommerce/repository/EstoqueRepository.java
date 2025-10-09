c13ea79ca5d27274dfbcc1d76e4d29f2e4802d86
DaviBeckha <davibeckhauser@hotmail.com>
2025-10-09 14:37:25 -0300

    Feat: Adicionado Busca por ID

--- ecommerce/src/main/java/dev/trier/ecommerce/repository/EstoqueRepository.java
+++ ecommerce/src/main/java/dev/trier/ecommerce/repository/EstoqueRepository.java
@@ -4,6 +4,9 @@ import dev.trier.ecommerce.model.EstoqueModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface EstoqueRepository extends JpaRepository<EstoqueModel, Integer> {
+    Optional<EstoqueModel> findByCdEstoque (Integer cdEstoque);
 }
--- ecommerce/src/main/java/dev/trier/ecommerce/repository/PedidoRepository.java
+++ ecommerce/src/main/java/dev/trier/ecommerce/repository/PedidoRepository.java
@@ -4,6 +4,9 @@ import dev.trier.ecommerce.model.PedidoModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PedidoRepository extends JpaRepository<PedidoModel, Integer> {
+    Optional<PedidoModel> findByCdPedido (Integer cdPedido);
 }
