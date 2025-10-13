869b94a5a14dade5bbb6decb99b97e1e36ab5e8a
DaviBeckha <davibeckhauser@hotmail.com>
2025-10-13 14:48:25 -0300

    Feat: Criado e não funcional ainda controller advice que vai ser usado para tratar exceções

---
+++ ecommerce/src/main/java/dev/trier/ecommerce/exception/GlobalExceptionHandler.java
@@ -0,0 +1,8 @@
+package dev.trier.ecommerce.exception;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+// Ele vai fazer as tratações de erro e enviar a mensagem por aqui deixando mais fácil e legível em vez de try/catch
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+}
