9370c4b54323ec8acc769df3ce9df43138794d3e
DaviBeckha <davibeckhauser@hotmail.com>
2025-10-13 14:50:48 -0300

    Feat: Criado somente as classes.

--- ecommerce/src/main/java/dev/trier/ecommerce/service/EmpresaService.java
+++ ecommerce/src/main/java/dev/trier/ecommerce/service/EmpresaService.java
@@ -1,6 +1,9 @@
 package dev.trier.ecommerce.service;
 
+import dev.trier.ecommerce.dto.empresa.criacao.EmpresaCriarDto;
+import dev.trier.ecommerce.model.EmpresaModel;
 import dev.trier.ecommerce.repository.EmpresaRepository;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,6 +15,19 @@ public class EmpresaService {
         this.empresaRepository = empresaRepository;
     }
 
+    public EmpresaModel criarEmpresa(EmpresaCriarDto dto) {
+        EmpresaModel empresaModel = new EmpresaModel();
+        empresaModel.setNmFantasia(dto.nmFantasia());
+        empresaModel.setNmRazao(dto.nmRazao());
+        empresaModel.setNuCPNJ(dto.nuCPNJ());
+        empresaModel.setNuTelefone(dto.nuTelefone());
+        empresaModel.setDsEndereco(dto.dsEndereco());
+        empresaModel.setNuEndereco(dto.nuEndereco());
 
-
+        try {
+            return empresaRepository.save(empresaModel);
+        } catch (DataIntegrityViolationException ex) {
+            throw ex;
+        }
+    }
 }
