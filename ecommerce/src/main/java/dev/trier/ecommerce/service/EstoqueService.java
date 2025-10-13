4e89c2ab93804c34d0cf3e224634a72565de58b7
DaviBeckha <davibeckhauser@hotmail.com>
2025-10-13 14:50:31 -0300

    Feat: Criado método de criar Estoque

--- ecommerce/src/main/java/dev/trier/ecommerce/service/EstoqueService.java
+++ ecommerce/src/main/java/dev/trier/ecommerce/service/EstoqueService.java
@@ -1,7 +1,39 @@
 package dev.trier.ecommerce.service;
 
+import dev.trier.ecommerce.dto.estoque.criacao.EstoqueCriarDto;
+import dev.trier.ecommerce.model.EstoqueModel;
+import dev.trier.ecommerce.model.ProdutoModel;
+import dev.trier.ecommerce.repository.EstoqueRepository;
+import dev.trier.ecommerce.repository.ProdutoRespository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class EstoqueService {
+
+    private final EstoqueRepository estoqueRepository;
+    private final ProdutoRespository produtoRespository;
+
+    public EstoqueService(EstoqueRepository estoqueRepository, ProdutoRespository produtoRespository) {
+        this.estoqueRepository = estoqueRepository;
+        this.produtoRespository = produtoRespository;
+    }
+
+    @Transactional
+    public EstoqueModel criarEstoque(EstoqueCriarDto estoqueCriarDto) {
+
+        ProdutoModel produtoModel = produtoRespository.findById(estoqueCriarDto.cdProduto())
+                .orElseThrow(() -> new ResponseStatusException(
+                        HttpStatus.NOT_FOUND,"Produto não encontrado para o código: " + estoqueCriarDto.cdProduto()));  //Procura cdProduto antes de criar no estoque
+
+        EstoqueModel estoqueModel = new EstoqueModel();
+        estoqueModel.setProduto(produtoModel);
+        estoqueModel.setQtdEstoqueProduto(estoqueCriarDto.qtdEstoqueProduto());
+
+        return estoqueRepository.save(estoqueModel);
+
+    }
 }
