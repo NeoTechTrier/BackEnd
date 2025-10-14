ef4839b07372a544aa6429179e2ac0c8488a2fc9
DaviBeckha <davibeckhauser@hotmail.com>
2025-10-14 11:16:22 -0300

    Feat: Criado filtros de buscas padrões

--- ecommerce/src/main/java/dev/trier/ecommerce/repository/ItemPedidoRepository.java
+++ ecommerce/src/main/java/dev/trier/ecommerce/repository/ItemPedidoRepository.java
@@ -4,6 +4,9 @@ import dev.trier.ecommerce.model.ItemPedidoModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ItemPedidoRepository extends JpaRepository<ItemPedidoModel, Integer> {
+    Optional<ItemPedidoModel> findByCdItemPedido(Integer cdItemPedido);
 }
--- ecommerce/src/main/java/dev/trier/ecommerce/repository/ProdutoRespository.java
+++ ecommerce/src/main/java/dev/trier/ecommerce/repository/ProdutoRespository.java
@@ -4,6 +4,9 @@ import dev.trier.ecommerce.model.ProdutoModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ProdutoRespository extends JpaRepository<ProdutoModel, Integer> {
+    Optional<ProdutoModel> findByCdProduto(Integer cdProduto);
 }
