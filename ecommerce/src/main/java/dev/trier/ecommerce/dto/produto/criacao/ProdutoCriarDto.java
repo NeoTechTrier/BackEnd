1920a580d5ea5fe5fd7437620940c2334f66b843
DaviBeckha <davibeckhauser@hotmail.com>
2025-10-16 11:05:19 -0300

    Feat: Mudado Patter Regex de categoria e Schemas para criação

--- ecommerce/src/main/java/dev/trier/ecommerce/dto/produto/criacao/ProdutoCriarDto.java
+++ ecommerce/src/main/java/dev/trier/ecommerce/dto/produto/criacao/ProdutoCriarDto.java
@@ -2,6 +2,7 @@ package dev.trier.ecommerce.dto.produto.criacao;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.*;
+import jakarta.websocket.OnMessage;
 import org.springframework.web.multipart.MultipartFile;
 @Schema(description = "DTO para criação de um novo produto.")
 public record ProdutoCriarDto(
@@ -16,18 +17,20 @@ public record ProdutoCriarDto(
         @Positive(message = "Valor do produto precisa ser positivo")
         Double vlProduto,
 
+        @Schema(description = "Categoria do produto. Escritas válidas: PLACA_DE_VIDEO, PROCESSADOR, RAM, HEADSET, TECLADO, MOUSE, MONITOR.", example = "PLACA_DE_VIDEO", requiredMode = Schema.RequiredMode.REQUIRED)
         @NotBlank(message = "Obrigatório preenchimento da categoria")
-        @Pattern(regexp = "^(PLACA_DE_VIDEO|PROCESSADOR|RAM|HEADSET|TECLADO|MOUSE|MONITOR)$", message = "Equipamento inválido. As opções válidas são: PLACA_DE_VIDEO, PROCESSADOR, RAM, HEADSET, TECLADO, MOUSE, MONITOR.")
+        @Pattern(regexp = "^(PLACADEVIDEO|PROCESSADOR|RAM|HEADSET|TECLADO|MOUSE|MONITOR)$", message = "Equipamento inválido. As opções válidas são: PLACA_DE_VIDEO, PROCESSADOR, RAM, HEADSET, TECLADO, MOUSE, MONITOR.")
         String dsCategoria,
 
-        @Schema(description = "Categoria do produto. Escritas válidas: PLACA_DE_VIDEO, PROCESSADOR, RAM, HEADSET, TECLADO, MOUSE, MONITOR.", example = "PLACA_DE_VIDEO", requiredMode = Schema.RequiredMode.REQUIRED)
+        @NotBlank(message = "Obrigatório a descrição do produto")
+        @Schema(description = "Descrição do produto, deixar descrito o produto neste campo para apresentar", example = "Memória Ram 32GB RedDragon", requiredMode = Schema.RequiredMode.REQUIRED)
         String dsProduto,
 
 
-
+        @Schema(description = "Imagem do produto que vai ser utilizado.", requiredMode = Schema.RequiredMode.REQUIRED)
         MultipartFile imgProduto,
 
 
-        @Schema(description = "Código identificador da empresa relacionada ao produto.", example = "101", requiredMode = Schema.RequiredMode.REQUIRED)
+        @Schema(description = "Código identificador da empresa relacionada ao produto.", example = "1", requiredMode = Schema.RequiredMode.REQUIRED)
         Integer cdEmpresa
 ) {}
