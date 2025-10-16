88ee110096cd0969822b318b93aff0b215a6fe76
Maicon Rocha <atendimento.maiconrs@gmail.com>
2025-10-16 09:41:16 -0300

    feat: adicionado novos metodos, dentre eles cadastrar empresa com verificação cnpj

--- ecommerce/src/main/java/dev/trier/ecommerce/service/EmpresaService.java
+++ ecommerce/src/main/java/dev/trier/ecommerce/service/EmpresaService.java
@@ -1,10 +1,13 @@
 package dev.trier.ecommerce.service;
 
+import dev.trier.ecommerce.dto.empresa.CriarEmpresaRequestDto;
+import dev.trier.ecommerce.dto.empresa.CriarEmpresaResponseDto;
 import dev.trier.ecommerce.model.EmpresaModel;
 import dev.trier.ecommerce.repository.EmpresaRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EmpresaService {
@@ -18,5 +21,44 @@ public class EmpresaService {
 
     public List<EmpresaModel> buscarEmpresas(){
         return empresaRepository.findAll();
+    }
+
+    //Buscar empresa pelo CdEmpresa;
+    public EmpresaModel findByCdEmprsa(Integer cdEmpresa){
+        //Procure um verificador com retorno de existencia, na qual entregara uma mensagem de existencia ou não. Exceções
+        Optional<EmpresaModel> empresa = empresaRepository.findByCdEmpresa(cdEmpresa);
+        return empresa.orElse(null);
+    }
+
+
+    public EmpresaModel findByNuCPNJ(String nuCPNJ){
+        Optional<EmpresaModel> empresa = empresaRepository.findByNuCPNJ(nuCPNJ);
+        return empresa.orElse(null);
+    }
+
+
+    public CriarEmpresaResponseDto cadastrarEmpresa( CriarEmpresaRequestDto request){
+        //Verificação de CNPJ no Banco
+        if (empresaRepository.findByNuCPNJ(request.nuCNPJ()).isPresent()){
+            throw new IllegalArgumentException("CNPJ já cadastrado");
+        }
+
+        EmpresaModel empresaModel= new EmpresaModel();
+        empresaModel.setNmFantasia(request.nmFantasia());
+        empresaModel.setNmRazao(request.nmRazao());
+        empresaModel.setNuCPNJ(request.nuCNPJ());
+        empresaModel.setNuTelefone(request.nuTelefone());
+        empresaModel.setDsEndereco(request.dsEndereco());
+        empresaModel.setNuEndereco(request.nuEndereco());
+        empresaModel.setFlAtivo(request.flAtivo());
+        EmpresaModel salvo = empresaRepository.save(empresaModel);
+
+        return new CriarEmpresaResponseDto(
+                salvo.getCdEmpresa(),
+                salvo.getNmFantasia(),
+                salvo.getNuCPNJ(),
+                salvo.getFlAtivo()
+        );
+
     }
 }
