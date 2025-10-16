c222d9b8eaafa84255ef296f4a369e847a36136b
Maicon Rocha <atendimento.maiconrs@gmail.com>
2025-10-16 09:32:47 -0300

    refactor: remoção e ajuste de métodos

--- ecommerce/src/main/java/dev/trier/ecommerce/repository/UsuarioRepository.java
+++ ecommerce/src/main/java/dev/trier/ecommerce/repository/UsuarioRepository.java
@@ -4,12 +4,12 @@ import dev.trier.ecommerce.model.UsuarioModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+
+import java.util.Optional;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<UsuarioModel, Integer> {
 
     //Verificação de existencia de cpf ao cadastrar novo usuario.
-    boolean existsByNuCPF(String nuCPF);
-    //List<UsuarioModel> findByNuCPF(String nuCPF);
+    Optional<UsuarioModel> findByNuCPF(String nuCPF);
 }
