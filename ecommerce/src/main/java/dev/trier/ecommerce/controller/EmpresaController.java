a7c853c298e8f3680118bda34a1bd8ff74e04a4a
Maicon Rocha <atendimento.maiconrs@gmail.com>
2025-10-16 09:43:52 -0300

    Feat: adicionado novos endpoints

--- ecommerce/src/main/java/dev/trier/ecommerce/controller/EmpresaController.java
+++ ecommerce/src/main/java/dev/trier/ecommerce/controller/EmpresaController.java
@@ -4,12 +4,14 @@ package dev.trier.ecommerce.controller;
 import dev.trier.ecommerce.dto.empresa.CriarEmpresaRequestDto;
 import dev.trier.ecommerce.dto.empresa.CriarEmpresaResponseDto;
 import dev.trier.ecommerce.model.EmpresaModel;
+import dev.trier.ecommerce.repository.EmpresaRepository;
 import dev.trier.ecommerce.service.EmpresaService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/empresa")
@@ -17,23 +19,47 @@ import java.util.List;
 public class EmpresaController {
 
     public final EmpresaService empresaService;
+    public final EmpresaRepository empresaRepository;
 
-    public EmpresaController(EmpresaService empresaService) {
+    public EmpresaController(EmpresaService empresaService , EmpresaRepository empresaRepository) {
         this.empresaService = empresaService;
+        this.empresaRepository = empresaRepository;
     }
+
 
     @GetMapping("/buscar")
     public List<EmpresaModel> buscarEmpresas(){
         return empresaService.buscarEmpresas();
     }
 
+
+    //Buscar empresa por CdEmpresa
+    @GetMapping("/{id}")
+    public ResponseEntity<EmpresaModel>  findById(@PathVariable Integer cdEmpresa){
+        EmpresaModel empresa = empresaService.findByCdEmprsa(cdEmpresa);
+        return  ResponseEntity.ok().body(empresa);
+    }
+
+
+    //Necessario efetuar teste de funcionalidade;
+    @GetMapping("/{nuCNPJ}")
+    public ResponseEntity<EmpresaModel>  findByNuCPNJ(@PathVariable String nuCNPJ){
+        EmpresaModel empresa = empresaService.findByNuCPNJ(nuCNPJ);
+        return  ResponseEntity.ok().body(empresa);
+    }
+
     /*
+    @GetMapping("/{cdEmpresa}")
+    public List<EmpresaModel> buscarEmpresasPorCdEmpresa(@PathVariable String cdEmpresa){
+        return empresaRepository.findByCdEmpresa(cdEmpresa);
+    }
+     */
+
+
     @PostMapping("/cadastrar")
     public ResponseEntity<CriarEmpresaResponseDto> cadastrarEmpresa(@RequestBody CriarEmpresaRequestDto criarEmpresaRequestDto){
         CriarEmpresaResponseDto criar = empresaService.cadastrarEmpresa(criarEmpresaRequestDto);
         return ResponseEntity.status(201).body(criar);
     }
-
-     */
 
 }
