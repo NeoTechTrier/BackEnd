9a5e54b5cea61a1194b9499fc7f462a4f751ea9b
Maicon Rocha <atendimento.maiconrs@gmail.com>
2025-10-16 09:39:59 -0300

    feat: somente comentarios

--- ecommerce/src/main/java/dev/trier/ecommerce/controller/ItemPedidoController.java
+++ ecommerce/src/main/java/dev/trier/ecommerce/controller/ItemPedidoController.java
@@ -8,4 +8,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/itempedido")
 @Tag(name = "Item Pedido", description = "Capacidade de criação e modificação do item pedido")
 public class ItemPedidoController {
+
+    //Metodo dedicado a salvar Todos os itens. Mas esses itens estão atralados ao pedido do cliente?
+    //Pq para salvar os itens, preciso de um usuario atual comprando =.
+
+    //Está classe está atralado a class Pedido, pois é dela que permite
+
+    //CalcularDescontoFinal
+
+    //Buscar Por Id
+
+    //Buscar Todos
+
+    //Salvar (Talvez possa salvar no carrinhou ou direto no carrinho )
 }
