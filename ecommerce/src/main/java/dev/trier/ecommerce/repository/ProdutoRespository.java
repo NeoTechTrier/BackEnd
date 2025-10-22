be8ff86e1f82bff5fd2b2551e4a40ce42ef45fde
DaviBeckha <davibeckhauser@hotmail.com>
2025-10-22 01:45:13 -0300

    Feat: Criado Query para trazer a categoria

--- ecommerce/src/main/java/dev/trier/ecommerce/repository/ProdutoRespository.java
+++ ecommerce/src/main/java/dev/trier/ecommerce/repository/ProdutoRespository.java
@@ -1,15 +1,24 @@
 package dev.trier.ecommerce.repository;
 
+import dev.trier.ecommerce.dto.produto.response.ProdutoIdResponseDto;
 import dev.trier.ecommerce.model.ProdutoModel;
+import dev.trier.ecommerce.model.enums.CategoriaProduto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import javax.swing.text.html.Option;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ProdutoRespository extends JpaRepository<ProdutoModel, Integer> {
     Optional<ProdutoModel> findByCdProduto(Integer cdProduto);
     Optional<ProdutoModel> findByNmProduto(String nmProduto);
-    Optional<ProdutoModel> findBydsCategoria(String dsCategoria);
+
+    @Query("SELECT new dev.trier.ecommerce.dto.produto.response.ProdutoIdResponseDto(" +
+            "p.nmProduto, p.vlProduto, p.dsProduto, p.dsCategoria) " +
+            "FROM ProdutoModel p WHERE p.dsCategoria = :categoria")
+    List<ProdutoIdResponseDto> findAllByDsCategoria(@Param("categoria") CategoriaProduto categoria);
+
 }
