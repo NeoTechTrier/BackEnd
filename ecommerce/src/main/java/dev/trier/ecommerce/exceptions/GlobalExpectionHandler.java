0c0b9585fe9e9126b1a1be9fdf78ca0cc52cb97d
DaviBeckha <davibeckhauser@hotmail.com>
2025-10-22 01:46:12 -0300

    Feat: Adicionado Crossorigin e criado um MaxUploadSizeExceededException para pegar as imagens com mais de 20mb

--- ecommerce/src/main/java/dev/trier/ecommerce/exceptions/GlobalExpectionHandler.java
+++ ecommerce/src/main/java/dev/trier/ecommerce/exceptions/GlobalExpectionHandler.java
@@ -6,15 +6,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
 
 import java.time.LocalDateTime;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
 @RestControllerAdvice
+@CrossOrigin("*")
 public class GlobalExpectionHandler {
 
-    @CrossOrigin
     @ExceptionHandler(RecursoNaoEncontradoException.class)
     public ResponseEntity<Object> handleRecursoNaoEncontrado(RecursoNaoEncontradoException ex) {
         Map<String, Object> body = new LinkedHashMap<>();
@@ -27,7 +28,7 @@ public class GlobalExpectionHandler {
 
 
     //Tratamento erro Global, busca automatico
-    @CrossOrigin
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Object> handleGenericException(Exception ex) {
         Map<String, Object> body = new LinkedHashMap<>();
@@ -36,5 +37,15 @@ public class GlobalExpectionHandler {
         body.put("error", "Erro interno no servidor ");
         body.put("message", ex.getMessage());
         return new ResponseEntity<>(body, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public ResponseEntity<Object> handleMaxUploadSizeExceeded(MaxUploadSizeExceededException ex) {
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("status", HttpStatus.PAYLOAD_TOO_LARGE.value());
+        body.put("error", "Arquivo muito grande");
+        body.put("message", "O tamanho do arquivo excede o limite permitido. Tente enviar um arquivo menor.");
+        return new ResponseEntity<>(body, HttpStatus.PAYLOAD_TOO_LARGE);
     }
 }
