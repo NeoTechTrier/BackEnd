ddad0dbcc43b0de14909efeee96403615e780adc
DaviBeckha <davibeckhauser@hotmail.com>
2025-10-24 01:47:35 -0300

    Feat: Continuar trabalhando pois não está funcionado @Put de produto

--- ecommerce/src/main/java/dev/trier/ecommerce/dto/produto/request/UpdateRequestDto.java
+++ ecommerce/src/main/java/dev/trier/ecommerce/dto/produto/request/UpdateRequestDto.java
@@ -1,36 +1,25 @@
 package dev.trier.ecommerce.dto.produto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.*;
 import org.springframework.web.multipart.MultipartFile;
 
 public record UpdateRequestDto(
-        Integer cdProduto,
-        @Schema(description = "Nome do produto.", example = "Placa de vídeo RTX 4090", requiredMode = Schema.RequiredMode.REQUIRED)
-        @NotBlank(message = "Obrigatório nome no produto")
-        @Size(min = 1, message = "O produto precisa ter mais de um caracter em seu nome")
+        @Schema(description = "Nome do produto.", example = "Placa de vídeo RTX 4090")
         String nmProduto,
 
-        @Schema(description = "Valor do produto em reais (precisa ser positivo).", example = "5999.90", requiredMode = Schema.RequiredMode.REQUIRED)
-        @NotNull(message = "Obrigatório colocar valor no produto")
-        @Positive(message = "Valor do produto precisa ser positivo")
+        @Schema(description = "Valor do produto em reais (precisa ser positivo).", example = "5999.90")
         Double vlProduto,
 
-        @Schema(description = "Categoria do produto. Escritas válidas: PLACA_DE_VIDEO, PROCESSADOR, RAM, HEADSET, TECLADO, MOUSE, MONITOR.", example = "PLACA_DE_VIDEO", requiredMode = Schema.RequiredMode.REQUIRED)
-        @NotBlank(message = "Obrigatório preenchimento da categoria")
-        @Pattern(regexp = "^(PLACADEVIDEO|PROCESSADOR|RAM|HEADSET|TECLADO|MOUSE|MONITOR)$", message = "Equipamento inválido. As opções válidas são: PLACA_DE_VIDEO, PROCESSADOR, RAM, HEADSET, TECLADO, MOUSE, MONITOR.")
+        @Schema(description = "Categoria do produto. Escritas válidas: PERIFERICOS, PROCESSADOR, RAM, GABINETE.", example = "RAM")
         String dsCategoria,
 
-        @NotBlank(message = "Obrigatório a descrição do produto")
-        @Schema(description = "Descrição do produto, deixar descrito o produto neste campo para apresentar", example = "Memória Ram 32GB RedDragon", requiredMode = Schema.RequiredMode.REQUIRED)
+        @Schema(description = "Descrição do produto", example = "Memória Ram 32GB RedDragon")
         String dsProduto,
 
+        @Schema(description = "Código identificador da empresa relacionada ao produto.", example = "1")
+        Integer cdEmpresa,
 
-        @Schema(description = "Imagem do produto que vai ser utilizado.", requiredMode = Schema.RequiredMode.REQUIRED)
-        MultipartFile imgProduto,
-
-
-        @Schema(description = "Código identificador da empresa relacionada ao produto.", example = "1", requiredMode = Schema.RequiredMode.REQUIRED)
-        Integer cdEmpresa
+        @Schema(description = "Imagem do produto que vai ser utilizado.")
+        MultipartFile imgProduto
 ) {
 }
