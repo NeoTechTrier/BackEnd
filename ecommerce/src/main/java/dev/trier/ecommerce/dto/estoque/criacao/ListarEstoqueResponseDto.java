d5c692ab9b6624d2bfe07040353ad8fdd3d51c14
DaviBeckha <davibeckhauser@hotmail.com>
2025-10-28 15:03:13 -0300

    Deleted: Flativo

--- ecommerce/src/main/java/dev/trier/ecommerce/dto/estoque/criacao/ListarEstoqueResponseDto.java
+++ ecommerce/src/main/java/dev/trier/ecommerce/dto/estoque/criacao/ListarEstoqueResponseDto.java
@@ -3,7 +3,6 @@ package dev.trier.ecommerce.dto.estoque.criacao;
 public record ListarEstoqueResponseDto(
         Integer cdEstoque,
         Integer qtdEstoqueProduto,
-        String flAtivo,
         Integer cdProduto,
         String nmProduto
 ) {
