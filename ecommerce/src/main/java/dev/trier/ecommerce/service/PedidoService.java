ebd1e9da06aa5fde7961d3b7556323134a8f4a84
Maicon Rocha <atendimento.maiconrs@gmail.com>
2025-10-29 22:41:26 -0300

    refactor: ajustes para teste

--- ecommerce/src/main/java/dev/trier/ecommerce/service/PedidoService.java
+++ ecommerce/src/main/java/dev/trier/ecommerce/service/PedidoService.java
@@ -1,12 +1,18 @@
 package dev.trier.ecommerce.service;
 
+import dev.trier.ecommerce.dto.itempedido.criacao.ItemPedidoCriarDto;
 import dev.trier.ecommerce.dto.pedido.criacao.ListarPedidosResponseDto;
 import dev.trier.ecommerce.dto.pedido.criacao.PedidoCriarDto;
 import dev.trier.ecommerce.dto.pedido.criacao.PedidoCriarResponseDto;
+import dev.trier.ecommerce.exceptions.RecursoNaoEncontradoException;
+import dev.trier.ecommerce.model.ItemPedidoModel;
 import dev.trier.ecommerce.model.PedidoModel;
+import dev.trier.ecommerce.model.ProdutoModel;
 import dev.trier.ecommerce.model.UsuarioModel;
 import dev.trier.ecommerce.repository.PedidoRepository;
+import dev.trier.ecommerce.repository.ProdutoRespository;
 import dev.trier.ecommerce.repository.UsuarioRepository;
+import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -18,8 +24,11 @@ public class PedidoService {
 
     private final PedidoRepository pedidoRepository;
     private final UsuarioRepository usuarioRepository;
+    private final ProdutoRespository produtoRespository;
+    private final EstoqueService estoqueService;
 
     //Metodo Criar Pedido
+    @Transactional
     public PedidoCriarResponseDto criarPedido(PedidoCriarDto pedidoCriarDto){
         UsuarioModel usarioModel = usuarioRepository.findById(pedidoCriarDto.cdUsuario()).orElseThrow(
                 () -> new RuntimeException("Usuário não encontrado para o código: " + pedidoCriarDto.cdUsuario()) // Procura usuário antes de criar o pedidoCriarDto
@@ -34,11 +43,64 @@ public class PedidoService {
         PedidoModel salvo = pedidoRepository.save(pedidoModel);
 
         return new PedidoCriarResponseDto(
+                salvo.getCdPedido(),
                 salvo.getFormaPagamento(),
                 salvo.getVlFrete(),
                 salvo.getVlTotalPedido()
         );
     }
+
+
+
+/*
+    @Transactional
+    public PedidoCriarResponseDto criarPedido(PedidoCriarDto pedidoCriarDto){
+        UsuarioModel usarioModel = usuarioRepository.findById(pedidoCriarDto.cdUsuario()).orElseThrow(
+                () -> new RuntimeException("Usuário não encontrado para o código: " + pedidoCriarDto.cdUsuario()) // Procura usuário antes de criar o pedidoCriarDto
+        );
+
+        PedidoModel pedidoModel = new PedidoModel();
+        pedidoModel.setUsuario(usarioModel);
+        pedidoModel.setFormaPagamento(pedidoCriarDto.formaPagamento());
+        pedidoModel.setVlFrete(pedidoCriarDto.vlFrete());
+        pedidoModel.setVlTotalPedido(pedidoCriarDto.vlTotalPedido());
+
+    ;
+
+        List<ItemPedidoModel> itens = new ArrayList<>();
+        for (ItemPedidoCriarDto itemDto : pedidoCriarDto.itensPedido()){
+           ProdutoModel produto = produtoRespository.findByCdProduto(itemDto.cdProduto())
+                    .orElseThrow(()-> new RecursoNaoEncontradoException("Produto não encontrado"));
+
+           //Lógica para consultar o estoque do Produtto
+           estoqueService.diminuirEstoqueProduto(produto.getCdProduto(), itemDto.qtItem());
+
+
+           ItemPedidoModel itemPedidoModel = new ItemPedidoModel();
+           itemPedidoModel.setProduto(produto);
+           itemPedidoModel.setQtItem(itemDto.qtItem());
+           itemPedidoModel.setPedido(pedidoModel);
+
+           itens.add(itemPedidoModel);
+        }
+
+        pedidoModel.setItensPedido(itens);
+        PedidoModel salvo = pedidoRepository.save(pedidoModel);
+
+        return new PedidoCriarResponseDto(
+                salvo.getFormaPagamento(),
+                salvo.getVlFrete(),
+                salvo.getVlTotalPedido()
+        );
+    }
+
+ */
+
+
+
+
+
+
 
     //Metodo Listar Pedidos //SOMENTE PARA ADMIN
     public List<ListarPedidosResponseDto> listarPedidos(){
@@ -52,9 +114,4 @@ public class PedidoService {
                 ))
                 .toList();
     }
-
-    //Metodo Finalizar Pedido
-
-
-    //Metodo Retirar Produto do Carrinho (Verificar se é em Pedido ou ItemPedido)
 }
