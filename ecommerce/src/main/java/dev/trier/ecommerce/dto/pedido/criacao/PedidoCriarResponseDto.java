36eba259e2676453d77d42a586a712b3ae932603
Maicon Rocha <atendimento.maiconrs@gmail.com>
2025-10-29 22:40:13 -0300

    refactor: adiconado cdPedido response

--- ecommerce/src/main/java/dev/trier/ecommerce/dto/pedido/criacao/PedidoCriarResponseDto.java
+++ ecommerce/src/main/java/dev/trier/ecommerce/dto/pedido/criacao/PedidoCriarResponseDto.java
@@ -3,6 +3,7 @@ package dev.trier.ecommerce.dto.pedido.criacao;
 import dev.trier.ecommerce.model.enums.FormaPagamento;
 
 public record PedidoCriarResponseDto(
+        Integer cdPedido,
         FormaPagamento formaPagamento,
         Double vlFrete,
         Double vlTotalPedido
